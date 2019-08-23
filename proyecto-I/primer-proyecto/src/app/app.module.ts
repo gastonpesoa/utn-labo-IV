39b61fd7d086184a4948e99043cfe438b762f096
gastonpesoa <gastonpesoa@gmail.com>
2019-08-22 22:03:24 -0300

    final de clase 1

--- proyecto-I/primer-proyecto/src/app/app.module.ts
+++ proyecto-I/primer-proyecto/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
@@ -14,7 +15,8 @@ import { LoginComponent } from './componentes/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- proyecto-I/primer-proyecto/src/app/componentes/login/login.component.ts
+++ proyecto-I/primer-proyecto/src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ususario } from '../../clases/ususario';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public usuario: Ususario;
+  public ingresado: Boolean;
+
+  constructor() { 
+    this.usuario = new Ususario();
+    this.usuario.nombre = "Pepe";
+    this.usuario.clave = "Secreto";
+    this.ingresado = false;
+  }
 
   ngOnInit() {
+  }
+
+  Ingresar(){
+    console.info("usuario: ", this.usuario);
+    this.ingresado = true;
   }
 
 }
