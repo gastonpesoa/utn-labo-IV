b9c043ef613a6dfff0895fc16fcb378be5887f53
gastonpesoa <gastonpesoa@gmail.com>
2019-09-21 10:20:31 -0300

    agrega tateti

--- TP-Sala-de-juegos-gastonpesoa/src/app/clases/juego-tateti.ts
+++ TP-Sala-de-juegos-gastonpesoa/src/app/clases/juego-tateti.ts
@@ -1,2 +1,105 @@
-export class JuegoTateti {
+import { Juego } from '../clases/juego'
+
+export class JuegoTateti extends Juego {
+    tablero: Array<Array<string>> = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+
+
+    jugada: number;
+    jugadaUsuario: number = 0;
+
+    juegoTerminado: boolean = true;
+
+    constructor(nombre?: string, gano?: boolean, jugador?: string) {
+        super("Ta Te Ti", true, jugador);
+    }
+
+    verificarTresEnLinea(marca: string): boolean {
+        if (//horizontales
+            (this.tablero[0][0] == marca && this.tablero[0][1] == marca && this.tablero[0][2] == marca) ||
+            (this.tablero[1][0] == marca && this.tablero[1][1] == marca && this.tablero[1][2] == marca) ||
+            (this.tablero[2][0] == marca && this.tablero[2][1] == marca && this.tablero[2][2] == marca) ||
+            //verticales
+            (this.tablero[0][0] == marca && this.tablero[1][0] == marca && this.tablero[2][0] == marca) ||
+            (this.tablero[0][1] == marca && this.tablero[1][1] == marca && this.tablero[2][1] == marca) ||
+            (this.tablero[0][2] == marca && this.tablero[1][2] == marca && this.tablero[2][2] == marca) ||
+            //diagonales
+            (this.tablero[0][0] == marca && this.tablero[1][1] == marca && this.tablero[2][2] == marca) ||
+            (this.tablero[0][2] == marca && this.tablero[1][1] == marca && this.tablero[2][0] == marca)) {
+            this.juegoTerminado = true;
+            if (marca == 'O')
+                this.gano = true;
+
+            // this.reset();
+        }
+        return this.juegoTerminado;
+    }
+
+    verificar(): boolean {
+        if (this.juegoTerminado && this.gano)
+            return true;
+        else
+            return false;
+    }
+
+    reset(): void {
+        this.gano = false;
+        this.tablero = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ];
+        console.info("tablero: ", this.tablero)
+    }
 }
+
+    // generarJugada(jugadaUsr) {
+
+    //     switch (jugadaUsr) {
+    //         case jugadaUsr[[0][0]]:
+    //             this.jugadas = [[0][1],[1][1],[2][0]]
+    //             this.jugada = this.jugadas[Math.floor(Math.random() * this.jugadas.length)];
+
+    //             row = (Math.random() * 2)
+    //             col = (Math.random() * 2)
+    //             break
+    //         case jugadaUsr[[0][1]]:
+    //             break
+    //         case jugadaUsr[[0][2]]:
+    //             break
+    //         case jugadaUsr[[1][0]]:
+    //             break
+    //         case jugadaUsr[[1][1]]:
+    //             break
+    //         case jugadaUsr[[1][2]]:
+    //             break
+    //         case jugadaUsr[[2][0]]:
+    //             break
+    //         case jugadaUsr[[2][1]]:
+    //             break
+    //         case jugadaUsr[[2][2]]:
+    //             break
+
+    //         case jugadaUsr[[0][0], [0][0]]:
+    //             break
+    //         case jugadaUsr[[0][0], [0][1]]:
+    //             break
+    //         case jugadaUsr[[0][0], [0][2]]:
+    //             break
+    //         case jugadaUsr[[0][0], [1][0]]:
+    //             break
+    //         case jugadaUsr[[0][0], [1][1]]:
+    //             break
+    //         case jugadaUsr[[0][0], [1][2]]:
+    //             break
+    //         case jugadaUsr[[0][0], [2][0]]:
+    //             break
+    //         case jugadaUsr[[0][0], [2][1]]:
+    //             break
+    //         case jugadaUsr[[0][0], [2][2]]:
+    //             break
+    //     }
+    // }
--- TP-Sala-de-juegos-gastonpesoa/src/app/componentes/tateti/tateti.component.ts
+++ TP-Sala-de-juegos-gastonpesoa/src/app/componentes/tateti/tateti.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { JuegoTateti } from '../../clases/juego-tateti';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-tateti',
@@ -6,8 +8,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tateti.component.css']
 })
 export class TatetiComponent implements OnInit {
+  nuevoJuego: JuegoTateti;
+  enJuego: boolean = false;
+  turnoJugador = false;
+  cuentaMarcas: number = 0;
+  imgCruz: string = '../../../assets/imagenes/cruz.png'
+  imgCirculo: string = '../../../assets/imagenes/circulo.png'
 
-  constructor() { }
+  constructor(private toastr: ToastrService) {
+    this.nuevoJuego = new JuegoTateti();
+  }
+
+  generarJugada() {
+    let row = Math.floor(Math.random() * 3);
+    let col = Math.floor(Math.random() * 3);
+    this.marcarJugada(row, col, true);
+  }
+
+
+  marcarJugada(row: number, column: number, jugadaGenerada: boolean) {
+    if (jugadaGenerada) {
+      if (this.nuevoJuego.tablero[row][column] != "" && this.cuentaMarcas < 9) {
+        this.generarJugada()
+      } else {
+        this.cuentaMarcas++;
+        this.nuevoJuego.tablero[row][column] = this.imgCruz;
+        this.turnoJugador = true;
+        if(this.nuevoJuego.verificarTresEnLinea(this.imgCruz)){
+          if(!this.nuevoJuego.verificar()){
+            this.toastr.error("Perdedor..", "Te derrotaron");
+          }
+        }
+      }
+    } else {
+      if (this.nuevoJuego.tablero[row][column] == "") {
+        this.cuentaMarcas++;
+        this.nuevoJuego.tablero[row][column] = this.imgCirculo;
+        this.turnoJugador = false;
+        if (!this.nuevoJuego.verificarTresEnLinea(this.imgCirculo)) {
+          setTimeout(() => {
+            this.generarJugada();
+          }, 400);
+        } else {
+          this.toastr.success("Felicitaciones!", "Ganaste esta vez");
+        }
+      }
+    }
+  }
+
+  nuevo() {
+    this.nuevoJuego.juegoTerminado = false;
+    this.nuevoJuego.reset();
+    this.enJuego = true;
+    this.turnoJugador = true;
+    this.cuentaMarcas = 0;
+  }
 
   ngOnInit() {
   }
